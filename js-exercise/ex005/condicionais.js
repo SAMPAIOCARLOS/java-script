ab6b6abb4ad353362a34aa63ed0bc89e1ca82bd3
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2023-06-19 22:11:00 -0300

    novos exercicios

---
+++ js-exercise/ex005/condicionais.js
@@ -0,0 +1,25 @@
+/*Condicionais
+ 
+*/
+
+const n1 = 5
+
+const n2 = 10 
+
+if(n1 < n2){
+    console.log('rapaz ele tá sem zap')
+}else{
+    console.log('rapaz ele tá com o zap')
+}
+
+
+
+let saldoBancario = 1000
+
+let contaParaPagar = 3500
+
+if(saldoBancario > contaParaPagar){
+    console.log('Sua conta foi paga com sucesso')
+}else{
+    console.log('Deixe de ser vagabundo e vai trabalhar para pagar suas contas')
+}
---
+++ js-exercise/ex005/variáveis.js
@@ -0,0 +1,38 @@
+//variáveis
+
+let n1 = 10
+
+console.log(n1)
+
+n1 = 20
+
+console.log(n1)
+
+n1 = 30
+
+console.log(n1)
+
+n1 = 40
+
+console.log(n1)
+
+n1 = 50
+
+console.log(n1)
+
+n1 = 60
+
+console.log(n1)
+
+n1 = 70
+
+console.log(n1)
+
+const n2 = 22
+
+n2 = 11
+
+console.log(n2)
+
+/*o var não é mais usado, o let é usado quando vc vai precisar mudar o valor da variável, o const é quando vc não vai mudar o valor da variável*/ 
+
