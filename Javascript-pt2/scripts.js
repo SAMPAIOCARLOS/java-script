a59ec43b79f16dd0b81f5c0678d1ac97eb5f69d9
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2023-08-23 16:02:29 -0300

    exercicio

---
+++ Javascript-pt2/scripts.js
@@ -0,0 +1,7 @@
+const showAllBututton = document.querySelector('.show-all') 
+
+function showAll(){
+    alert("chamei")
+}
+
+showAllBututton.addEventListener('click', showAll)
---
+++ javascript-pt1/function.js
@@ -0,0 +1,36 @@
+// console.log("um")
+// console.log("dois")
+// console.log("tres")
+// console.log("quatro")
+
+function soma(valor1, valor2){
+    const resultado = valor1 + valor2
+    return resultado
+}
+
+function multiplicar(valor1, valor2){
+    const resultado =valor1 * valor2
+    return resultado
+}
+
+function subitracao(valor1, valor2){
+    const resultado = valor1 - valor2
+    return resultado
+}
+
+function divisao(valor1, valor2){
+    const resultado = valor1 / valor2
+    return resultado
+}
+
+const resultadoDaSoma = soma(5,5)
+console.log(resultadoDaSoma)
+
+const resultadoDaMultiplicacao = multiplicar(5,5)
+console.log(resultadoDaMultiplicacao)
+
+const resultadoDaSubitracao = subitracao(5,5)
+console.log(resultadoDaSubitracao)
+
+const resultadoDaDivisao = divisao(5,5)
+console.log(resultadoDaDivisao)
